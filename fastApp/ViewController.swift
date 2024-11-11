0a021319e51a375ada6683bc6c5ba54e08c9295e
ikdoo1004 <ikdoo1004@gmail.com>
2024-11-11 15:57:34 -0500

    Color theme decided
    GottaGoFast app is based on timer function to login (10 seconds), register (999 seconds), and no timer after all (as UX, no reason to set timer). Also it's just a test implementaiton.

--- fastApp/ViewController.swift
+++ fastApp/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    struct User: Codable {
+        let name: String
+        let email: String
+        let phone: String
+        let password: String
+    }
+    
+    var newName = ""
+    var newEmail = ""
+    var newPhone = ""
+    var newPassword = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-/*
+    /*
+    @IBAction func nameInput(_ sender: Any) {
+    }
+    @IBAction func emailInput(_ sender: Any) {
+    }
+    @IBAction func phoneInput(_ sender: Any) {
+    }
+    @IBAction func passwordInput(_ sender: Any) {
+    }
+    */
+    @IBAction func registerButton(_ sender: Any) {
+        if newName.isEmpty || newEmail.isEmpty || newPhone.isEmpty || newPassword.isEmpty {
+            let alert = UIAlertController(title: "Alert", message: "Please fill in all the fields.", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: NSLocalizedString("Close", comment: "This closes alert"), style: .default, handler: { _ in
+                NSLog("The \"OK\" alert occured.")
+            }
+                                         )
+            )
+        } else {
+            User.init(name: newName, email: newEmail, phone: newPhone, password: newPassword)
+            let note = "\(newName) has been registered.\nPlease login to your account."
+            
+        }
+    }
+    /*
     @IBAction func registerNotAvailable(_ sender: Any) {
         let alert = UIAlertController(title: "Alert", message: "No Register process is yet available.", preferredStyle: .alert)
 
