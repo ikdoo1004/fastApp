2fe3614543947cc4005103d180a67007dfd03ca7
ikdoo1004 <98782167+ikdoo1004@users.noreply.github.com>
2024-11-11 13:49:16 -0500

    First commit
    first push

---
+++ fastApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fastApp
+//
+//  Created by 김익두 on 11/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
