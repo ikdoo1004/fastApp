2f856a521cdc826211ac59317260c85d5b210df1
ikdoo1004 <ikdoo1004@gmail.com>
2024-11-11 13:55:48 -0500

    Alert UI
    When press Register button, AlertControl will be called.

--- fastApp/ViewController.swift
+++ fastApp/ViewController.swift
@@ -14,6 +14,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func registerNotAvailable(_ sender: Any) {
+        let alert = UIAlertController(title: "Alert", message: "No Register process is yet available.", preferredStyle: .alert)
 
+                // You can add actions using the following code
+                alert.addAction(UIAlertAction(title: NSLocalizedString("Close", comment: "This closes alert"), style: .default, handler: { _ in
+                NSLog("The \"OK\" alert occured.")
+                }))
+
+                // This part of code inits alert view
+                self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
