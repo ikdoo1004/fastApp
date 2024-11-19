de8ab9ce5bd1a132358774b9e5a1067ae628307f
ikdoo1004 <ikdoo1004@gmail.com>
2024-11-19 16:49:53 +0900

    Table View Cell studied; constraints will be adjusted soon.

---
+++ fastApp/CustomCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomCell.swift
+//  fastApp
+//
+//  Created by 김익두 on 11/19/24.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ fastApp/Songs.swift
@@ -0,0 +1,12 @@
+//
+//  Employee.swift
+//  fastApp
+//
+//  Created by 김익두 on 11/19/24.
+//
+
+struct Employee{
+    var id: Int
+    let name: String
+    var age: Int
+}
