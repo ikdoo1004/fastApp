c0899e0a863a4650801204a445c338720f62cb39
ikdoo1004 <ikdoo1004@gmail.com>
2024-11-22 23:48:28 +0900

    Registration/storing User info implemented. Login verification also is completed.

---
+++ fastApp/Accounts.swift
@@ -0,0 +1,8 @@
+//
+//  Accounts.swift
+//  fastApp
+//
+//  Created by 김익두 on 11/22/24.
+//
+
+import Foundation
---
+++ fastApp/OTPViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OTPViewController.swift
+//  fastApp
+//
+//  Created by 김익두 on 11/22/24.
+//
+
+import UIKit
+
+class OTPViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
